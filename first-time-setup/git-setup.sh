a82166bf40783054ed316f5d78dad8ba58b4ed0e
Norman Wong <nich19032@outlook.com>
2020-06-03 14:52:38 +1000

    Remove parameters from git-setup script

--- first-time-setup/git-setup.sh
+++ first-time-setup/git-setup.sh
@@ -1,10 +1,5 @@
 # Setting up git on a new machine
 
-#Scripts Parameters
-USER_NAME=$1
-USER_EMAIL=$2
-GPG_KEY=$3
-
 git config --global user.name "$USER_NAME"
 git config --global user.email $USER_EMAIL
 git config --global core.editor "code --wait"
