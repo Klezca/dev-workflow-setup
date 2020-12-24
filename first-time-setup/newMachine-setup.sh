1701790b68978402fd114ea32680606ca53975c8
Norman Wong <nich19032@outlook.com>
2020-12-24 09:36:59 +0800

    Updated initial setup script
    - Updated node version
    - Updated python version
    - Added yarn, php and git package

--- first-time-setup/newMachine-setup.sh
+++ first-time-setup/newMachine-setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Setup Script to be executed every time I work on a new machine
-# Distribution: WSL Ubuntu 18.04
+# Distribution: WSL Ubuntu 20.04
 
 # Add python and ansible repo to system repo
 sudo apt-add-repository --yes --update ppa:deadsnakes/ppa
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 
 # Add node repo to system repo
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
 
-# Update System
-sudo apt-get -y update && sudo apt-get -y upgrade
+# Add yarn package manager to system repo
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 
 # Install essential dependencies
 sudo apt-get -y install \
@@ -23,22 +23,42 @@ sudo apt-get -y install \
     software-properties-common \
     build-essential \
     git-flow \
+    git \
     gnupg2 \
-    ansible
+    ansible \
+    php
 
-# Install node and npm
+# Update System
+sudo apt-get -y update && sudo apt-get -y upgrade
+
+# Install node, npm and yarn
 sudo apt-get install -y nodejs
 sudo apt install node
+sudo apt install yarn
 
-# Install python3.8 and pip3 and Python headers
+# Install python3.9 and pip3 and Python headers
 sudo apt-get -y install \
-    python3.8 \
-    python3.8-venv \
+    python3.9 \
+    python3.9-venv \
     python3-pip \
-    python3.8-dev
+    python3.9-dev
 
-# # Install aws cli v2
+# Install common PHP 7.4 modules
+sudo apt-get install php-pear \
+    php-fpm \ 
+    php-dev \
+    php-zip \
+    php-curl \
+    php-xmlrpc \
+    php-gd \
+    php-mysql \
+    php-mbstring \
+    php-xml \
+    libapache2-mod-php
+
+# Install aws cli v2
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo unzip awscliv2.zip
 sudo ./aws/install
 rm -rf aws awscliv2.zip
+
